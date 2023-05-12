877be1e04194ec8e500158ffb437218180e622fa
Gabryel-lima <gabbryellimasi@gmail.com>
2023-05-12 20:15:13 -0300

    Atualizando

--- AP Lógica De Programação/EX3 Refatorado.py
+++ AP Lógica De Programação/EX3 Refatorado.py
@@ -1,4 +1,32 @@
 class Limpeza:
 
     def __init__(self):
-        
+        pass
+
+
+
+
+
+
+    def valida_valor_metragem(self):
+        pass
+
+
+    def valida_tipo_limpeza(self):
+        pass
+
+
+    def testa_adicional_limpeza(self):
+        pass
+
+
+    def exibe_valores(self):
+        pass
+
+
+
+if __name__ == '__main__':
+
+    print('\nBem-vindo ao Programa de serviços de Limpeza do Gabryel Lima Da Silva')
+    print(80 * '*')
+    
